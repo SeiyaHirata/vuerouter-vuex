7471cb39e28f2533c9d80f44c39319f32eb3f757
“SeiyaHirata” <s-hirata@g-account.page>
2021-02-25 16:06:11 +0000

    change_router-view

--- src/App.vue
+++ src/App.vue
@@ -34,21 +34,15 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
+  components: {},
   data: () => ({
     //
   })
