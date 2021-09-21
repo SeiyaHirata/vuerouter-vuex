633f571064143c64c50e4d6063283471dbd0f1d4
“SeiyaHirata” <s-hirata@g-account.page>
2021-09-22 04:48:49 +0900

    見た目整え

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -4,18 +4,22 @@
       <v-progress-circular indeterminate size="64"></v-progress-circular>
     </v-overlay>
     <v-card class="mx-auto mt-5" max-width="344" v-else>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <v-list-item-title class="headline mb-1">
-            {{ event.name }}
-          </v-list-item-title>
-          <v-list-item-subtitle class="my-5">詳細</v-list-item-subtitle>
-          <v-list-item-subtitle>{{ event.detail }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+      <v-card-text>
+        <p class="display-1 text--primary">
+          {{ event.name }}
+        </p>
+        <div class="text--primary">開始日 {{ event.start }}</div>
+        <div class="text--primary">終了日 {{ event.end }}</div>
+        <p class="display-1 text--primary">
+          詳細
+        </p>
+        <div class="text--primary">
+          {{ event.detail ? event.detail : "詳細なし" }}
+        </div>
+      </v-card-text>
 
       <v-card-actions>
-        <v-chip class="ma-2" :color="event.color">
+        <v-chip class="ma-2 white--text" :color="event.color">
           {{ event.color }}
         </v-chip>
         <router-link to="/">ホームに戻る</router-link>
