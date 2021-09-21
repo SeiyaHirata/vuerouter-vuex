054ce30297cfccbc31e6e759cfc6a5ada7e56a79
“SeiyaHirata” <s-hirata@g-account.page>
2021-09-22 04:28:39 +0900

    ローディング追加

--- src/store/event.js
+++ src/store/event.js
@@ -3,7 +3,8 @@ import { getEventAll } from "@/service/event";
 export const event = {
   namespaced: true,
   state: {
-    events: []
+    events: [],
+    isLoading: false
   },
   getters: {
     event: state => id => {
@@ -16,17 +17,23 @@ export const event = {
     },
     updateEvents(state, value) {
       state.events.push(value);
+    },
+    updateIsLoading(state, value) {
+      state.isLoading = value;
     }
   },
   actions: {
     // イベント一覧取得
     async getEventAll({ commit }) {
       try {
+        commit("updateIsLoading", true);
         const response = await getEventAll();
         commit("updateEventsAll", response.data);
       } catch (error) {
         alert("エラーが発生しました");
         commit("updateEventsAll", []);
+      } finally {
+        commit("updateIsLoading", false);
       }
     },
     createEvent({ commit }, payload) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,48 @@
 <template>
   <div class="home">
-    <v-sheet tile height="54" class="d-flex">
-      <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-btn icon class="ma-2" @click="$refs.calendar.next()">
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-      <v-btn
-        color="primary"
-        fab
-        dense
-        small
-        class="ma-2"
-        @click="switchCreateEventDialog(true)"
-      >
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <v-select
-        v-model="type"
-        :items="types"
-        dense
-        outlined
-        hide-details
-        class="ma-2"
-        label="type"
-      ></v-select>
-    </v-sheet>
-    <v-sheet height="600">
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :type="type"
-        :events="events"
-        :event-overlap-threshold="30"
-        @click:event="toEventPage"
-      ></v-calendar>
-    </v-sheet>
+    <v-overlay v-if="isLoading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+    <div v-else>
+      <v-sheet tile height="54" class="d-flex">
+        <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn icon class="ma-2" @click="$refs.calendar.next()">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+        <v-btn
+          color="primary"
+          fab
+          dense
+          small
+          class="ma-2"
+          @click="switchCreateEventDialog(true)"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-select
+          v-model="type"
+          :items="types"
+          dense
+          outlined
+          hide-details
+          class="ma-2"
+          label="type"
+        ></v-select>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="value"
+          :type="type"
+          :events="events"
+          :event-overlap-threshold="30"
+          @click:event="toEventPage"
+        ></v-calendar>
+      </v-sheet>
+    </div>
   </div>
 </template>
 
@@ -59,6 +64,11 @@ export default {
     events: {
       get() {
         return this.$store.state.event.events;
+      }
+    },
+    isLoading: {
+      get() {
+        return this.$store.state.event.isLoading;
       }
     }
   },
